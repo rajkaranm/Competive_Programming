844f626702fd81dded56e9bfdcfa8f9b1783014b
Rajkaran <rajkaranvir456@gmail.com>
2023-01-07 18:30:11 +0530

    Solved more questions

---
+++ Longest-Consecutive-Sequence/longestConsectiveSequence.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> set;
+        int longest = 0;
+        int size = nums.size();
+
+        for(int i = 0; i < size; i++) {
+            set.insert(nums[i]);
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (set.find(nums[i] - 1) == set.end()) {
+                int length = 1;
+                while (set.find(nums[i] + length) != set.end()) {
+                    length++;
+                }
+
+                longest = max(longest, length);
+            }
+        }
+        return longest;
+        
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nums = {100, 4, 200, 1, 3, 2};
+    int ans = s.longestConsecutive(nums);
+    cout<<ans<<endl;
+    return 0;
+}
